ba90b92b43b6b0721a8067f602d1a3afce3dcf9f
172476032 <172476032@qq.com>
2019-03-07 17:41:30 +0800

    首次提交

---
+++ src/components/sceneMap/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    asdas
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  destroyed() {}
+};
+</script>
+
+<style lang="scss">
+</style>
